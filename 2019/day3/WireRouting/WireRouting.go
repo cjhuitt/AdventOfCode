0151c880bacd1bdb2431caa8bd0f7e39466be2bd
Caleb Huitt <chuitt@agleader.com>
2020-11-25 14:03:17 -0600

    Initial idea of path generation, testing containment

--- 2019/day3/WireRouting/WireRouting.go
+++ 2019/day3/WireRouting/WireRouting.go
@@ -8,6 +8,19 @@ func Default() path {
 	return path{[]node{}}
 }
 
+func Route(r string) path {
+	return path{[]node{}}
+}
+
 func (p path) Length() int {
 	return len(p.path)
+}
+
+func (p path) Contains(n node) bool {
+	for _, test := range p.path {
+		if n.EqualTo(test) {
+			return true
+		}
+	}
+	return false
 }
--- 2019/day3/WireRouting/WireRouting_test.go
+++ 2019/day3/WireRouting/WireRouting_test.go
@@ -8,3 +8,25 @@ func TestDefaultPathHasNoLength(t *testing.T) {
 		t.Errorf("Default().Length() == %d, want 0", got)
 	}
 }
+
+func TestBuildsPathFromInstructions(t *testing.T) {
+	tests := []struct {
+		path     string
+		length   int
+		contains []node
+	}{
+		{path: "", length: 0, contains: []node{}},
+	}
+	for i, tc := range tests {
+		r := Route(tc.path)
+		got := r.Length()
+		if got != tc.length {
+			t.Errorf("Route(%v).Length) want %d, got %d (case %d)", tc.path, tc.length, got, i)
+		}
+		for _, n := range tc.contains {
+			if !r.Contains(n) {
+				t.Errorf("Route(%v).Contains(%v) want true, got false (case %d)", tc.path, n, i)
+			}
+		}
+	}
+}
