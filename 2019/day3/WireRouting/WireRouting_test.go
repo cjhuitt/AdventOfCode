4596aeeff24f8ac2379b442c3cda39303d718d22
Caleb Huitt <chuitt@agleader.com>
2020-11-25 15:20:27 -0600

    Test bigger intersection cases and getting the closest intersection

--- 2019/day3/WireRouting/WireRouting_test.go
+++ 2019/day3/WireRouting/WireRouting_test.go
@@ -57,3 +57,32 @@ func TestIntersections(t *testing.T) {
 		}
 	}
 }
+
+func TestClosestIntersections(t *testing.T) {
+	tests := []struct {
+		first  string
+		second string
+		want   int
+	}{
+		{first: "R75,D30,R83,U83,L12,D49,R71,U7,L72", second: "U62,R66,U55,R34,D71,R55,D58,R83", want: 159},
+		{first: "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51", second: "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7", want: 135},
+	}
+	for i, tc := range tests {
+		one := Route(tc.first)
+		two := Route(tc.second)
+		got := one.Intersections(two)
+		if len(got) == 0 {
+			t.Errorf("Route(%v).Intersections(Route(%v)) expected intersections, got none (case %d)", tc.first, tc.second, i)
+		}
+		closest := got[0].ManhattanLength()
+		for _, n := range got {
+			d := n.ManhattanLength()
+			if d < closest {
+				closest = d
+			}
+		}
+		if closest != tc.want {
+			t.Errorf("Route(%v).Intersections(Route(%v)) want closest intersection %d away, got %d (case %d)", tc.first, tc.second, tc.want, closest, i)
+		}
+	}
+}
