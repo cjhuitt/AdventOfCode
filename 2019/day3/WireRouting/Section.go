0db35ea0cc8131a1dade1b148ef5f482639b3a63
Caleb Huitt <chuitt@agleader.com>
2020-11-26 13:38:59 -0600

    Hardcoded intersect function

--- 2019/day3/WireRouting/Section.go
+++ 2019/day3/WireRouting/Section.go
@@ -5,3 +5,7 @@ type section struct{ a, b node }
 func (s section) ManhattanLength() int {
 	return abs(s.a.x-s.b.x) + abs(s.a.y-s.b.y)
 }
+
+func (s section) Intersect(other section) (bool, node) {
+	return true, node{}
+}
--- 2019/day3/WireRouting/Section_test.go
+++ 2019/day3/WireRouting/Section_test.go
@@ -21,3 +21,22 @@ func TestLengths(t *testing.T) {
 		}
 	}
 }
+
+func TestIntersect(t *testing.T) {
+	tests := []struct {
+		a, b, c, d, want node
+		want_found       bool
+	}{
+		{a: Node(1, 1), b: Node(-1, -1), c: Node(1, -1), d: Node(-1, 1), want: Node(0, 0), want_found: true},
+	}
+	for i, tc := range tests {
+		s1 := section{tc.a, tc.b}
+		s2 := section{tc.c, tc.d}
+		found, got := s1.Intersect(s2)
+		if found != tc.want_found {
+			t.Errorf("Section(%v, %v).Intersect() want found = %v, got %v (case %d)", tc.a, tc.b, tc.want, got, i)
+		} else if found && tc.want != got {
+			t.Errorf("Section(%v, %v).Intersect() want %v, got %v (case %d)", tc.a, tc.b, tc.want, got, i)
+		}
+	}
+}
