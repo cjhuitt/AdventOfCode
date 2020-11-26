6815d3be71780a47866e82a3f1cac9ac5816fa85
Caleb Huitt <chuitt@agleader.com>
2020-11-26 13:58:26 -0600

    Rework initial test case for simple non-intersection

--- 2019/day3/WireRouting/Section.go
+++ 2019/day3/WireRouting/Section.go
@@ -6,6 +6,21 @@ func (s section) ManhattanLength() int {
 	return abs(s.a.x-s.b.x) + abs(s.a.y-s.b.y)
 }
 
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+// WARNING: Only works for vertical or horizontal line segments
 func (s section) Intersect(other section) (bool, node) {
-	return true, node{}
+	return false, node{}
 }
--- 2019/day3/WireRouting/Section_test.go
+++ 2019/day3/WireRouting/Section_test.go
@@ -27,16 +27,16 @@ func TestIntersect(t *testing.T) {
 		a, b, c, d, want node
 		want_found       bool
 	}{
-		{a: Node(1, 1), b: Node(-1, -1), c: Node(1, -1), d: Node(-1, 1), want: Node(0, 0), want_found: true},
+		{a: Node(1, 0), b: Node(-1, 0), c: Node(1, 1), d: Node(-1, 1), want: node{}, want_found: false},
 	}
 	for i, tc := range tests {
 		s1 := section{tc.a, tc.b}
 		s2 := section{tc.c, tc.d}
 		found, got := s1.Intersect(s2)
 		if found != tc.want_found {
-			t.Errorf("Section(%v, %v).Intersect() want found = %v, got %v (case %d)", tc.a, tc.b, tc.want, got, i)
+			t.Errorf("Section(%v, %v).Intersect(Section(%v, %v)) want found = %v, got %v (case %d)", tc.a, tc.b, tc.c, tc.d, tc.want_found, found, i)
 		} else if found && tc.want != got {
-			t.Errorf("Section(%v, %v).Intersect() want %v, got %v (case %d)", tc.a, tc.b, tc.want, got, i)
+			t.Errorf("Section(%v, %v).Intersect(Section(%v, %v)) want %v, got %v (case %d)", tc.a, tc.b, tc.c, tc.d, tc.want, got, i)
 		}
 	}
 }
