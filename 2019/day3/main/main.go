320d966f08572e00c3f2cb288a46c2b7b493d2c3
Caleb Huitt <chuitt@agleader.com>
2020-11-26 15:25:06 -0600

    Fix main to use renamed function

--- 2019/day3/main/main.go
+++ 2019/day3/main/main.go
@@ -33,7 +33,7 @@ func main() {
 
 	r0 := WireRouting.Route(paths[0])
 	r1 := WireRouting.Route(paths[1])
-	p := WireRouting.Closest(r0.Intersections(r1))
+	p := WireRouting.ClosestPhysical(r0.Intersections(r1))
 
 	fmt.Println("Closest intersection at", p, "distance", p.ManhattanLength())
 }
