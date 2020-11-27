7730e99976bbacb746f035260f1f36900dd768ed
Caleb Huitt <chuitt@agleader.com>
2020-11-26 21:01:48 -0600

    Multi-place generators

--- 2019/day4/main.go
+++ 2019/day4/main.go
@@ -27,11 +27,30 @@ func (g generator) ResetTo(min int) generator {
 	return generator{min, min, g.mult}
 }
 
+type generators struct {
+	places [6]generator
+}
+
+func InitGenerators(hun_k, ten_k, k, hun, ten, one int) generators {
+	a := generator{0, 0, 100000}.ResetTo(hun_k)
+	b := generator{0, 0, 10000}.ResetTo(ten_k)
+	c := generator{0, 0, 1000}.ResetTo(k)
+	d := generator{0, 0, 100}.ResetTo(hun)
+	e := generator{0, 0, 10}.ResetTo(ten)
+	f := generator{0, 0, 1}.ResetTo(one)
+
+	return generators{[6]generator{a, b, c, d, e, f}}
+}
+
+func (set generators) GeneratedValue() int {
+	v := 0
+	for _, g := range set.places {
+		v += g.Value()
+	}
+	return v
+}
+
 func main() {
-	tens := generator{1, 2, 10}
-	fmt.Println(tens, tens.Value())
-	tens = tens.Step()
-	fmt.Println(tens, tens.Value())
-	tens = tens.ResetTo(3)
-	fmt.Println(tens, tens.Value())
+	g := InitGenerators(1, 2, 8, 3, 9, 2)
+	fmt.Println(g.GeneratedValue())
 }
