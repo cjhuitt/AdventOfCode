e7396124a115e46d61a2e50c0eb8b1e312bd9250
Caleb Huitt <chuitt@agleader.com>
2020-11-28 15:20:07 -0600

    First successful parse

--- 2019/day6/orbit/orbit.go
+++ 2019/day6/orbit/orbit.go
@@ -1,6 +1,9 @@
 package orbit
 
-import "errors"
+import (
+	"errors"
+	"strings"
+)
 
 type body struct {
 	steps  int
@@ -8,5 +11,10 @@ type body struct {
 }
 
 func Parse(code string) (string, string, error) {
-	return "", "", errors.New("fail")
+	parts := strings.Split(code, ")")
+	if len(parts) != 2 {
+		return "", "", errors.New("fail")
+	}
+
+	return parts[0], parts[1], nil
 }
--- 2019/day6/orbit/orbit_test.go
+++ 2019/day6/orbit/orbit_test.go
@@ -3,8 +3,31 @@ package orbit
 import "testing"
 
 func TestErrorsParsingEmptyString(t *testing.T) {
-	a, b, err := Parse("")
+	c, id, err := Parse("")
 	if err == nil {
-		t.Errorf("Expected parsing %#v to error, got %#v, %#v", "", a, b)
+		t.Errorf("Expected parsing %#v to error, got %#v, %#v", "", c, id)
+	}
+}
+
+func TestParsing(t *testing.T) {
+	tests := []struct {
+		input       string
+		want_center string
+		want_id     string
+		errors      bool
+	}{
+		{input: "COM)B", want_center: "COM", want_id: "B", errors: false},
+	}
+	for i, tc := range tests {
+		c, id, err := Parse(tc.input)
+		if tc.errors && err == nil {
+			t.Errorf("Expected parsing %#v to error, got %#v, %#v (case %d)", tc.input, c, id, i)
+		}
+		if !tc.errors && err != nil {
+			t.Errorf("Expected parsing %#v to succeed, got %v (case %d)", tc.input, err, i)
+		}
+		if c != tc.want_center || id != tc.want_id {
+			t.Errorf("Expected Parse(%v) to give (%#v, %#v), got (%#v, %#v) (case %d)", tc.input, tc.want_center, tc.want_id, c, id, i)
+		}
 	}
 }
