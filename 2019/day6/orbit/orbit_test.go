32993ea95442479d356b67d2d7976009f60d3d69
Caleb Huitt <chuitt@agleader.com>
2020-11-28 15:21:33 -0600

    More test parses

--- 2019/day6/orbit/orbit_test.go
+++ 2019/day6/orbit/orbit_test.go
@@ -17,16 +17,17 @@ func TestParsing(t *testing.T) {
 		errors      bool
 	}{
 		{input: "COM)B", want_center: "COM", want_id: "B", errors: false},
+		{input: "B)C", want_center: "B", want_id: "C", errors: false},
+		{input: "D)I", want_center: "D", want_id: "I", errors: false},
+		{input: "ASDF", want_center: "", want_id: "", errors: true},
 	}
 	for i, tc := range tests {
 		c, id, err := Parse(tc.input)
 		if tc.errors && err == nil {
 			t.Errorf("Expected parsing %#v to error, got %#v, %#v (case %d)", tc.input, c, id, i)
-		}
-		if !tc.errors && err != nil {
+		} else if !tc.errors && err != nil {
 			t.Errorf("Expected parsing %#v to succeed, got %v (case %d)", tc.input, err, i)
-		}
-		if c != tc.want_center || id != tc.want_id {
+		} else if c != tc.want_center || id != tc.want_id {
 			t.Errorf("Expected Parse(%v) to give (%#v, %#v), got (%#v, %#v) (case %d)", tc.input, tc.want_center, tc.want_id, c, id, i)
 		}
 	}
