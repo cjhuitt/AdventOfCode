6b19300414a6ae0bba4912ea91f4a3248e07eb92
Caleb Huitt <chuitt@agleader.com>
2020-12-02 09:45:49 -0600

    Parse spans

--- 2020/day2/pwparser/pwparser.go
+++ 2020/day2/pwparser/pwparser.go
@@ -1,7 +1,25 @@
 package pwparser
 
+import (
+	"errors"
+	"strconv"
+	"strings"
+)
+
 type span struct{ min, max int }
 
 func parseSpan(in string) (span, error) {
-	return span{1, 3}, nil
+	parts := strings.Split(in, "-")
+	if len(parts) != 2 {
+		return span{}, errors.New("Invalid range specification")
+	}
+	min, err := strconv.Atoi(parts[0])
+	if err != nil {
+		return span{}, err
+	}
+	max, err := strconv.Atoi(parts[1])
+	if err != nil {
+		return span{}, err
+	}
+	return span{min, max}, nil
 }
--- 2020/day2/pwparser/pwparser_test.go
+++ 2020/day2/pwparser/pwparser_test.go
@@ -2,6 +2,10 @@ package pwparser
 
 import "testing"
 
+func equalSpan(a, b span) bool {
+	return a.min == b.min && a.max == b.max
+}
+
 func TestParseRange(t *testing.T) {
 	tests := []struct {
 		input    string
@@ -9,13 +13,14 @@ func TestParseRange(t *testing.T) {
 		want_err bool
 	}{
 		{input: "1-3", want: span{1, 3}, want_err: false},
+		{input: "2-9", want: span{2, 9}, want_err: false},
 	}
 	for i, tc := range tests {
 		got, err := parseSpan(tc.input)
 		if !tc.want_err && err != nil {
 			t.Errorf("Expected parse(%v) to work, received error %v (case %d)", tc.input, err, i)
-		} else if tc.want.min != got.min || tc.want.max != got.max {
-			t.Errorf("Expected parse(%v) to result in %d, %d, received %d, %d (case %d)", tc.input, tc.want.min, tc.want.max, got.min, got.max, i)
+		} else if !equalSpan(tc.want, got) {
+			t.Errorf("Expected parse(%v) to result in %v, received %v (case %d)", tc.input, tc.want, got, i)
 		}
 	}
 }
