c8aa2e2d65cbad1fbe1a47795f85df27ee7fdfd3
Caleb Huitt <chuitt@agleader.com>
2020-12-02 07:51:59 -0600

    Initial files

---
+++ 2020/day2/pwparser/pwparser.go
@@ -0,0 +1,7 @@
+package pwparser
+
+import "fmt"
+
+func main() {
+	fmt.Println("vim-go")
+}
---
+++ 2020/day2/pwparser/pwparser_test.go
@@ -0,0 +1,7 @@
+package pwparser
+
+import "testing"
+
+func TestHelloWorld(t *testing.T) {
+	// t.Fatal("not implemented")
+}
