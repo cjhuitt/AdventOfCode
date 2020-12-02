c57db9c97bd8ce4a0fa97eb898974c550b0c6359
Caleb Huitt <chuitt@agleader.com>
2020-12-02 09:59:25 -0600

    Read the password candidates

--- 2020/day2/main.go
+++ 2020/day2/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	file, err := os.Open("passwords.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	candidates := make([]string, 0)
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		candidates = append(candidates, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Candidates:", len(candidates))
 }
