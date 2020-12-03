266feca80268c87136ca7d81d58ab436c9ce37b9
Caleb Huitt <chuitt@agleader.com>
2020-12-03 11:33:03 -0600

    Track multiple paths

--- 2020/day3/main.go
+++ 2020/day3/main.go
@@ -14,21 +14,37 @@ func main() {
 	}
 	defer file.Close()
 
-	trees := 0
-	pos := 0
+	trees := [5]int{}
+	pos := [5]int{}
+	move_r := [5]int{1, 3, 5, 7, 1}
+	move_d := [5]int{1, 1, 1, 1, 2}
+	lineno := 0
+
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := scanner.Text()
-		pos = pos % len(line)
-		if line[pos] == '#' {
-			trees++
+		fmt.Println(lineno%move_d[4], lineno%move_d[3])
+		for i := 0; i < len(pos); i++ {
+			pos[i] = pos[i] % len(line)
+			if lineno%move_d[i] == 0 {
+				if (line[pos[i]]) == '#' {
+					trees[i]++
+				}
+
+				pos[i] += move_r[i]
+			}
 		}
-		pos += 3
+		lineno++
 	}
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println("Encountered", trees, "trees")
+	mult := 1
+	for i := 0; i < len(pos); i++ {
+		fmt.Println("For slope r", move_r[i], "d", move_d[i], "encountered", trees[i], "trees")
+		mult *= trees[i]
+	}
+	fmt.Println(mult, "trees")
 }
