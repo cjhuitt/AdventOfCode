ae61d3489584f8830eb9eb7b52bb291971176ea4
Caleb Huitt <chuitt@agleader.com>
2020-12-05 11:53:10 -0600

    Track occupied status of seats, find unoccupied one

--- 2020/day5/main.go
+++ 2020/day5/main.go
@@ -16,17 +16,30 @@ func main() {
 	}
 	defer file.Close()
 
+	occupied := make(map[int]bool)
 	max := -1
+	min := 999
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		id := seats.Find(scanner.Text()).Id()
+		occupied[id] = true
 		if id > max {
 			max = id
+		}
+		if id < min {
+			min = id
 		}
 	}
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
+	}
+
+	for i := min; i < max; i++ {
+		if !occupied[i] {
+			fmt.Println("Unoccupied:", i)
+			break
+		}
 	}
 
 	fmt.Println("Max id:", max)
