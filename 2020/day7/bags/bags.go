c55c1ff7f5517ec5f7720039d1b74d0ac5e14c99
Caleb Huitt <chuitt@agleader.com>
2020-12-09 11:49:23 -0600

    Parse basic non-containing bag

--- 2020/day7/bags/bags.go
+++ 2020/day7/bags/bags.go
@@ -1,12 +1,25 @@
 package bags
 
+import (
+	"strings"
+)
+
 type bag struct {
 	style    string
 	contents map[string]int
 }
 
+func parseStyle(in string) string {
+	return strings.TrimSuffix(in, " bags")
+}
+
 func Parse(in string) bag {
-	return bag{}
+	parts := strings.Split(in, " contain ")
+	if len(parts) != 2 {
+		return bag{}
+	}
+
+	return bag{style: parseStyle(parts[0])}
 }
 
 func (b bag) isEqualTo(other bag) bool {
