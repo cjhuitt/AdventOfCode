b64be014b3dd6209bafbb6ce10821fb14c4025d3
Caleb Huitt <chuitt@agleader.com>
2020-12-09 17:42:18 -0600

    Remove extraneous output

--- 2020/day8/main.go
+++ 2020/day8/main.go
@@ -32,21 +32,17 @@ func main() {
 
 	program = orig
 	test, program, found := orig.FixNextNop(0)
-	fmt.Println("Next", test)
 	good := false
 	last := orig
 	for found && !good {
 		good, last = program.Execute()
 		test, program, found = orig.FixNextNop(test + 1)
-		fmt.Println("Next", test)
 	}
 
 	test, program, found = orig.FixNextJmp(0)
-	fmt.Println("Next", test)
 	for found && !good {
 		good, last = program.Execute()
 		test, program, found = orig.FixNextJmp(test + 1)
-		fmt.Println("Next", test)
 	}
 
 	if good {
