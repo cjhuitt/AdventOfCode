f076ddb3858edef542b374d45c3c1e74c6c811a8
Caleb Huitt <chuitt@agleader.com>
2020-12-09 11:41:19 -0600

    Fill in struct and comparison

--- 2020/day7/bags/bags.go
+++ 2020/day7/bags/bags.go
@@ -1,11 +1,23 @@
 package bags
 
-type bag struct{ row, col, id int }
+type bag struct {
+	style    string
+	contents map[string]int
+}
 
 func Parse(in string) bag {
 	return bag{}
 }
 
 func (b bag) isEqualTo(other bag) bool {
+	if b.style != other.style || len(b.contents) != len(other.contents) {
+		return false
+	}
+
+	for style, count := range b.contents {
+		if val, ok := other.contents[style]; !ok || val != count {
+			return false
+		}
+	}
 	return true
 }
