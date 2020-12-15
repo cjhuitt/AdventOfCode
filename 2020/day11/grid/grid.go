f6a960294b789d45d5ff2e98bce5ddcd9e357f5f
Caleb Huitt <chuitt@agleader.com>
2020-12-15 14:42:13 -0600

    Assign neighbors

--- 2020/day11/grid/grid.go
+++ 2020/day11/grid/grid.go
@@ -58,19 +58,126 @@ func readSeating(in []string) deck {
 	return s
 }
 
-func (s deck) isEqualTo(other deck) bool {
-	if len(s.seats) != len(other.seats) {
+func (d deck) isEqualTo(other deck) bool {
+	if len(d.seats) != len(other.seats) {
 		return false
 	}
-	for i := 0; i < len(s.seats); i++ {
-		if !seatSlicesEqual(s.seats[i], other.seats[i]) {
+	for i := 0; i < len(d.seats); i++ {
+		if !seatSlicesEqual(d.seats[i], other.seats[i]) {
 			return false
 		}
 	}
 	return true
 }
 
+func (d deck) northwestOf(i, j int) *seat {
+	if i < 0 || j < 0 {
+		return nil
+	}
+	return &d.seats[i-1][j-1]
+}
+
+func (d deck) northOf(i, j int) *seat {
+	if j < 0 {
+		return nil
+	}
+	return &d.seats[i][j-1]
+}
+
+func (d deck) northeastOf(i, j int) *seat {
+	if i >= d.width || j < 0 {
+		return nil
+	}
+	return &d.seats[i+1][j-1]
+}
+
+func (d deck) eastOf(i, j int) *seat {
+	if i >= d.width {
+		return nil
+	}
+	return &d.seats[i+1][j]
+}
+
+func (d deck) southeastOf(i, j int) *seat {
+	if i >= d.width || j >= d.height {
+		return nil
+	}
+	return &d.seats[i+1][j+1]
+}
+
+func (d deck) southOf(i, j int) *seat {
+	if j >= d.height {
+		return nil
+	}
+	return &d.seats[i][j+1]
+}
+
+func (d deck) southwestOf(i, j int) *seat {
+	if i < 0 || j >= d.height {
+		return nil
+	}
+	return &d.seats[i-1][j+1]
+}
+
+func (d deck) westOf(i, j int) *seat {
+	if i < 0 {
+		return nil
+	}
+	return &d.seats[i-1][j]
+}
+
+func (d deck) neighborsOf(i, j int) []*seat {
+	r := []*seat{}
+
+	n := d.northwestOf(i, j)
+	if n != nil {
+		r = append(r, n)
+	}
+
+	n = d.northOf(i, j)
+	if n != nil {
+		r = append(r, n)
+	}
+
+	n = d.northeastOf(i, j)
+	if n != nil {
+		r = append(r, n)
+	}
+
+	n = d.eastOf(i, j)
+	if n != nil {
+		r = append(r, n)
+	}
+
+	n = d.southeastOf(i, j)
+	if n != nil {
+		r = append(r, n)
+	}
+
+	n = d.southOf(i, j)
+	if n != nil {
+		r = append(r, n)
+	}
+
+	n = d.southwestOf(i, j)
+	if n != nil {
+		r = append(r, n)
+	}
+
+	n = d.westOf(i, j)
+	if n != nil {
+		r = append(r, n)
+	}
+
+	return r
+}
+
 func Parse(in []string) deck {
 	init := readSeating(in)
+	for i := 0; i < init.width; i++ {
+		for j := 0; j < init.width; j++ {
+			init.seats[i][j].neighbors = init.neighborsOf(i, j)
+		}
+	}
 	return init
 }
