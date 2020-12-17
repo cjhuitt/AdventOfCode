a864a8ca540ee33f0f09fb2885523034b85f3bd0
Caleb Huitt <chuitt@agleader.com>
2020-12-17 13:59:06 -0600

    Refactor turning

--- 2020/day12/routing/router.go
+++ 2020/day12/routing/router.go
@@ -25,6 +25,36 @@ func (s ship) newPos(dir byte, steps int) loc {
 	return s.pos
 }
 
+func rightFrom(orient byte) byte {
+	switch orient {
+	case 'N':
+		return 'E'
+	case 'E':
+		return 'S'
+	case 'S':
+		return 'W'
+	case 'W':
+		return 'N'
+	}
+
+	return orient
+}
+
+func leftFrom(orient byte) byte {
+	switch orient {
+	case 'N':
+		return 'W'
+	case 'E':
+		return 'N'
+	case 'S':
+		return 'E'
+	case 'W':
+		return 'S'
+	}
+
+	return orient
+}
+
 func (s ship) Moved(in string) ship {
 	if len(in) < 2 {
 		return s
@@ -44,29 +74,11 @@ func (s ship) Moved(in string) ship {
 		r.pos = s.newPos(r.orient, v)
 	case 'R':
 		for i := 0; i < v/90; i++ {
-			switch r.orient {
-			case 'N':
-				r.orient = 'E'
-			case 'E':
-				r.orient = 'S'
-			case 'S':
-				r.orient = 'W'
-			case 'W':
-				r.orient = 'N'
-			}
+			r.orient = rightFrom(r.orient)
 		}
 	case 'L':
 		for i := 0; i < v/90; i++ {
-			switch r.orient {
-			case 'N':
-				r.orient = 'W'
-			case 'E':
-				r.orient = 'N'
-			case 'S':
-				r.orient = 'E'
-			case 'W':
-				r.orient = 'S'
-			}
+			r.orient = leftFrom(r.orient)
 		}
 	}
 	return r
