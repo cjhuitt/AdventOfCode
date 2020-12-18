be3993c68d1aa4f4af941e9fd523b6c33f905ccb
Caleb Huitt <chuitt@agleader.com>
2020-12-18 10:25:31 -0600

    Adapt test to include starting position

--- 2020/day12/routing/router_test.go
+++ 2020/day12/routing/router_test.go
@@ -4,24 +4,25 @@ import "testing"
 
 func TestStepped(t *testing.T) {
 	tests := []struct {
-		move string
-		want ship
+		start ship
+		move  string
+		want  ship
 	}{
-		{move: "", want: ship{loc{0, 0}, loc{10, 0}}},
-		{move: "N1", want: ship{loc{0, 1}, loc{10, 0}}},
-		{move: "E1", want: ship{loc{1, 0}, loc{10, 0}}},
-		{move: "W1", want: ship{loc{-1, 0}, loc{10, 0}}},
-		{move: "S1", want: ship{loc{0, -1}, loc{10, 0}}},
-		{move: "R90", want: ship{loc{0, 0}, loc{0, -10}}},
-		{move: "R180", want: ship{loc{0, 0}, loc{-10, 0}}},
-		{move: "R270", want: ship{loc{0, 0}, loc{0, 10}}},
-		{move: "L90", want: ship{loc{0, 0}, loc{0, 10}}},
-		{move: "F1", want: ship{loc{10, 0}, loc{10, 0}}},
+		{start: Ship(), move: "", want: ship{loc{0, 0}, loc{10, 0}}},
+		{start: Ship(), move: "N1", want: ship{loc{0, 1}, loc{10, 0}}},
+		{start: Ship(), move: "E1", want: ship{loc{1, 0}, loc{10, 0}}},
+		{start: Ship(), move: "W1", want: ship{loc{-1, 0}, loc{10, 0}}},
+		{start: Ship(), move: "S1", want: ship{loc{0, -1}, loc{10, 0}}},
+		{start: Ship(), move: "R90", want: ship{loc{0, 0}, loc{0, -10}}},
+		{start: Ship(), move: "R180", want: ship{loc{0, 0}, loc{-10, 0}}},
+		{start: Ship(), move: "R270", want: ship{loc{0, 0}, loc{0, 10}}},
+		{start: Ship(), move: "L90", want: ship{loc{0, 0}, loc{0, 10}}},
+		{start: Ship(), move: "F1", want: ship{loc{10, 0}, loc{10, 0}}},
 	}
 	for i, tc := range tests {
-		got := Ship().Stepped(tc.move)
+		got := tc.start.Stepped(tc.move)
 		if tc.want != got {
-			t.Errorf("Ship().Stepped(%v) want %v, got %v (case %d)", tc.move, tc.want, got, i)
+			t.Errorf("%v.Stepped(%v) want %v, got %v (case %d)", tc.start, tc.move, tc.want, got, i)
 		}
 	}
 }
