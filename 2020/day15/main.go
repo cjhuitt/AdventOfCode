809f0c6b27c78c16a506c66639b66f957ca530e0
Caleb Huitt <chuitt@agleader.com>
2020-12-22 10:30:33 -0600

    Track turns

--- 2020/day15/main.go
+++ 2020/day15/main.go
@@ -5,7 +5,8 @@ import (
 )
 
 type game struct {
-	mem map[int]int
+	mem  map[int]int
+	turn int
 }
 
 func NewGame(start []int) game {
@@ -14,21 +15,23 @@ func NewGame(start []int) game {
 	for i, n := range start {
 		g.mem[n] = i + 1
 	}
+	g.turn = len(g.mem) + 1
 	return g
 }
 
 func (g *game) Turn() int {
-	return 0
+	return g.turn
 }
 
 func (g *game) NextTurn() int {
+	g.turn++
 	return 0
 }
 
 func play(start []int, turns int) {
 	g := NewGame(start)
 	last := 0
-	for g.Turn() > turns {
+	for g.Turn() <= turns {
 		last = g.NextTurn()
 	}
 
