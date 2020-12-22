3257268028efec14bcb326851ed5c59cb9dacc7f
Caleb Huitt <chuitt@agleader.com>
2020-12-22 09:36:05 -0600

    Seed the initial game values

--- 2020/day15/main.go
+++ 2020/day15/main.go
@@ -6,8 +6,18 @@ import (
 
 func play(start []int, turns int) {
 	fmt.Println("Starting with", start, "after", turns, "turns the number is")
+	fmt.Println()
 }
 
 func main() {
-	play([]int{1, 3, 2}, 2020)
+	play([]int{0, 3, 6}, 2020)
+	//play([]int{1, 3, 2}, 2020)
+	//play([]int{2, 1, 3}, 2020)
+	//play([]int{1, 2, 3}, 2020)
+	//play([]int{2, 3, 1}, 2020)
+	//play([]int{3, 2, 1}, 2020)
+	//play([]int{3, 1, 2}, 2020)
+
+	//fmt.Println()
+	//play([]int{9, 19, 1, 6, 0, 5, 4}, 2020)
 }
