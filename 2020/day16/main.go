fa4d7405ce01e1edf4acd4b5ef5b92b966c97cc5
Caleb Huitt <chuitt@agleader.com>
2020-12-23 14:25:43 -0600

    Parse specs and tickets

--- 2020/day16/main.go
+++ 2020/day16/main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 	"os"
+
+	"tickets"
 )
 
 func countLines(infile string) {
@@ -14,12 +16,47 @@ func countLines(infile string) {
 	}
 	defer file.Close()
 
+	specs := []tickets.FieldSpec{}
 	lines := 0
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "" {
+			break
+		}
+		specs = append(specs, tickets.ParseFieldSpec(line))
 		lines++
 	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
 
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "your ticket:" {
+			continue
+		}
+		if line == "" {
+			break
+		}
+		lines++
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	others := []tickets.Ticket{}
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "nearby tickets:" {
+			continue
+		}
+		if line == "" {
+			break
+		}
+		others = append(others, tickets.ParseTicket(line))
+		lines++
+	}
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
