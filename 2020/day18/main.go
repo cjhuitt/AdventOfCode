3c808a16a2dc347e981fbc79960279e5ccbdd1ac
Caleb Huitt <chuitt@agleader.com>
2020-12-25 15:43:53 -0600

    Parse addition

--- 2020/day18/main.go
+++ 2020/day18/main.go
@@ -5,7 +5,41 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
+	"text/scanner"
 )
+
+func calculate(in string) int {
+	var s scanner.Scanner
+	s.Init(strings.NewReader(in))
+	var operand int
+	var op string
+	var err error
+	for tok := s.Scan(); tok != scanner.EOF; tok = s.Scan() {
+		t := s.TokenText()
+		switch t {
+		case "+":
+			op = "+"
+		default:
+			if op == "" {
+				operand, err = strconv.Atoi(t)
+				if err != nil {
+					log.Fatal(err)
+				}
+			} else {
+				var b int
+				b, err = strconv.Atoi(t)
+				if err != nil {
+					log.Fatal(err)
+				}
+				operand = operand + b
+			}
+		}
+	}
+	fmt.Println(in, "=", operand)
+	return operand
+}
 
 func countLines(infile string) {
 	file, err := os.Open(infile)
@@ -28,5 +62,6 @@ func countLines(infile string) {
 }
 
 func main() {
-	countLines("input.txt")
+	calculate("1 + 2")
+	//countLines("input.txt")
 }
