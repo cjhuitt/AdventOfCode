335f41adbae2a2c00c8d5730b63b14b1dc9705f0
Caleb Huitt <chuitt@agleader.com>
2020-12-25 15:30:12 -0600

    Bootstrap code

---
+++ 2020/day18/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func countLines(infile string) {
+	file, err := os.Open(infile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	lines := 0
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines++
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(infile, ":", lines, "lines found")
+}
+
+func main() {
+	countLines("input.txt")
+}
