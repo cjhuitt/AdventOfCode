658c569a4f651d181a0191ab5513125a99738839
Caleb Huitt <chuitt@agleader.com>
2022-12-05 12:03:12 -0600

    Day 5 step 2

--- 2022/day05/cargo.rb
+++ 2022/day05/cargo.rb
@@ -1,28 +1,40 @@
-stacks = [Array.new, Array.new, Array.new, Array.new, Array.new,
-          Array.new, Array.new, Array.new, Array.new]
+stacks1 = [Array.new, Array.new, Array.new, Array.new, Array.new,
+           Array.new, Array.new, Array.new, Array.new]
+stacks2 = [Array.new, Array.new, Array.new, Array.new, Array.new,
+           Array.new, Array.new, Array.new, Array.new]
 initializing = true
 
 File.foreach("input.txt").with_index do |line, index|
   unless line.strip.empty?
     if initializing
       vals = line.scan /.(.)...(.)...(.)...(.)...(.)...(.)...(.)...(.)...(.)./m
+      puts "#{vals}"
       vals.flatten.each.with_index do |val, index|
-        stacks[index].push val if not val.strip.empty?
+        stacks1[index].push val if not val.strip.empty?
+        stacks2[index].push val if not val.strip.empty?
       end
     else
       a, b, c = line.scan /(\d+)/
       count, from, to = a.join.to_i, b.join.to_i - 1, c.join.to_i - 1
-      shift = stacks[from].pop count
-      shift.reverse.each do |crate|
-        stacks[to].push crate
+      shift1 = stacks1[from].pop count
+      shift2 = stacks2[from].pop count
+      shift1.reverse.each do |crate|
+        stacks1[to].push crate
+      end
+      shift2.each do |crate|
+        stacks2[to].push crate
       end
     end
   else
     initializing = false
-    stacks.each do |stack|
+    stacks1.each do |stack|
+      stack.reverse!
+    end
+    stacks2.each do |stack|
       stack.reverse!
     end
   end
 end
 
-puts "Tops: #{stacks.map{|s| s.last}.join}"
+puts "First Tops: #{stacks1.map{|s| s.last}.join}"
+puts "Second Tops: #{stacks2.map{|s| s.last}.join}"
