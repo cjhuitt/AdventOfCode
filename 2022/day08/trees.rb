1ee6dc7c604906b7b715e5b352e067b2999f54f2
Caleb Huitt <chuitt@agleader.com>
2022-12-09 15:10:43 -0600

    Day 8 part 2

--- 2022/day08/trees.rb
+++ 2022/day08/trees.rb
@@ -1,13 +1,15 @@
 class Node
-  attr_reader :height
+  attr_reader :height, :row, :col
   attr_accessor :up, :down, :right, :left
 
-  def initialize( h )
+  def initialize( r, c, h )
+    @row = r
+    @col = c
     @height = h
-    @up = nil
-    @down = nil
-    @right = nil
-    @left = nil
+  end
+
+  def to_s
+    "Node(#{row},#{col}): #{height}"
   end
 
   def connect_up( node )
@@ -26,6 +28,11 @@ class Node
     return true if downward.max < height
     return true if rightward.max < height
     leftward.max < height
+  end
+
+  def scenic_score
+    return 0 if edge?
+    upward_vis * downward_vis * leftward_vis * rightward_vis
   end
 
   private
@@ -58,24 +65,53 @@ class Node
   def leftward
     dirward {|n| n.left}
   end
+
+  def dirward_vis(&block)
+    c = 0
+    node = block.call(self)
+    while node and node.height < height
+      c += 1
+      node = block.call(node)
+    end
+    c += 1 if node
+    c
+  end
+
+  def upward_vis
+    dirward_vis {|n| n.up}
+  end
+
+  def downward_vis
+    dirward_vis {|n| n.down}
+  end
+
+  def rightward_vis
+    dirward_vis {|n| n.right}
+  end
+
+  def leftward_vis
+    dirward_vis {|n| n.left}
+  end
 end
 
 nodes = []
 last_row = []
 
-File.foreach("input.txt", chomp: true) do |line|
+File.foreach("input.txt", chomp: true).with_index do |line, row|
   heights = line.split("")
-  row = []
-  heights.each.with_index do |height, index|
-    node = Node.new( height.to_i )
-    node.connect_up( last_row[index] )
-    node.connect_left( row.last )
-    row << node
+  row_nodes = []
+  heights.each.with_index do |height, col|
+    node = Node.new( row, col, height.to_i )
+    node.connect_up( last_row[col] )
+    node.connect_left( row_nodes.last )
+    row_nodes << node
     nodes << node
   end
-  last_row = row
+  last_row = row_nodes
 end
 
 visible = nodes.reject { |n| not n.visible? }
 puts "Visible: #{visible.count}"
 
+scenic_scores = nodes.collect { |n| n.scenic_score }
+puts "Highest Scenic Score: #{scenic_scores.max}"
