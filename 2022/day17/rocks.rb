54d95d5408c44009a1fe873afdd2152bebfc3b7f
Caleb Huitt <developer-caleb@huitt.org>
2022-12-19 19:12:16 -0600

    Refactor to have a column class

--- 2022/day17/rocks.rb
+++ 2022/day17/rocks.rb
@@ -32,61 +32,68 @@ class Rock
 
   def move(jet, column)
     try = @position + MOVES[jet]
-    @position = try if not blocked?(try, column)
+    @position = try if not column.blocked? @offsets.collect {|o| o + try}
     orig = @position
     try = @position + MOVES[:down]
-    @position = try if not blocked?(try, column)
+    @position = try if not column.blocked? @offsets.collect {|o| o + try}
     orig.y != @position.y
   end
 
   def to_s
     "Rock at [#{@offsets.collect{|o| "#{o + @position}"}.join(", ")}]]"
   end
+end
 
-  private
-  def blocked?(pos, column)
-    @offsets.any? do |offset|
-      check = offset + pos
-      return true if check.y == -1
-      return true if check.x == -1
-      return true if check.x == 7
-      column[check.y][check.x]
+class Column
+  attr_reader :space
+  def initialize
+    @space = Array.new(11000) {Array.new(7) {false}}
+    @top = -1
+  end
+
+  def blocked?(coords)
+    coords.any? do |coord|
+      return true if coord.y == -1
+      return true if coord.x == -1
+      return true if coord.x == 7
+      @space[coord.y][coord.x]
     end
+  end
+
+  def fixate(rock)
+    rows = [@top]
+    rock.offsets.each do |offset|
+      p = rock.position + offset
+      @space[p.y][p.x] = true
+      rows << p.y
+    end
+    @top = rows.max
+  end
+
+  def highest
+    @top + 1
+  end
+
+  def output(lines)
+    (0..lines).each do |row|
+      print "|#{@space[lines - row].collect {|v| v ? "#" : "."}.join}|\n"
+    end
+    print "+-------+"
   end
 end
 
 rock_cycle = [:dash, :plus, :reverse_l, :capital_i, :block]
-column = Array.new(11000) {Array.new(7) {false}}
+column = Column.new
 input = ARGV.fetch(0, "input.txt")
 jets = File.open(input, chomp: true, &:readline).strip
 
-def print_column(column, lines)
-  (0..lines).each do |row|
-    print "|#{column[lines - row].collect {|v| v ? "#" : "."}.join}|\n"
-  end
-  print "+-------+"
-end
-
-def fixate(rock, column)
-  rows = []
-  rock.offsets.each do |offset|
-    p = rock.position + offset
-    column[p.y][p.x] = true
-    rows << p.y
-  end
-  rows
-end
-
-top = -1
 jet_index = 0
 
-(0..2021).each do |count|
-  rock = Rock.new(rock_cycle[count % rock_cycle.length], Point.new(2, top + 4))
-  while rock.move(jets[jet_index % jets.length], column)
-    jet_index += 1
-  end
+(0...2022).each do |count|
+  rock = Rock.new(rock_cycle[count % rock_cycle.length], Point.new(2, column.highest + 3))
+  jet_index += 1 while rock.move(jets[jet_index % jets.length], column)
+  column.fixate(rock)
   jet_index += 1
-  top = [top, fixate(rock, column).max].max
 end
 
-puts "Column reached #{top + 1}"
+puts "Column reached #{column.highest}"
