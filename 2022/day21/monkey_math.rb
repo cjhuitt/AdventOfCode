de6dbffb6d7bc58235d92a16b5c8be732ff6f97c
Caleb Huitt <developer-caleb@huitt.org>
2022-12-28 09:51:31 -0600

    Day 21 part 2

--- 2022/day21/monkey_math.rb
+++ 2022/day21/monkey_math.rb
@@ -1,5 +1,5 @@
 class Monkey
-  attr_reader :name
+  attr_reader :name, :operation
   def initialize(name, operation)
     @name = name
     @operation = operation
@@ -10,11 +10,11 @@ class Monkey
   end
 
   def resolve(monkeys)
-    return @operation.to_i if @operation.match? /\d+/
-    op = @operation.match /(?<a>\w+) (?<op>.) (?<b>\w+)/
-    a = monkeys[op[:a]].resolve(monkeys)
-    b = monkeys[op[:b]].resolve(monkeys)
-    case op[:op]
+    return @operation.to_i if @operation.to_s.match? /\d+/
+    parts = op_parts(monkeys)
+    a = parts[0].resolve(monkeys)
+    b = parts[2].resolve(monkeys)
+    case parts[1]
     when "+"
       a + b
     when "-"
@@ -24,6 +24,11 @@ class Monkey
     when "/"
       a / b
     end
+  end
+
+  def op_parts(monkeys)
+    op = @operation.match /(?<a>\w+) (?<op>.) (?<b>\w+)/
+    [monkeys[op[:a]], op[:op], monkeys[op[:b]]]
   end
 end
 
@@ -35,4 +40,54 @@ File.foreach(input, chomp: true).with_index do |line, index|
   monkeys[m.name] = m
 end
 
-puts "root: #{monkeys['root'].resolve(monkeys)}"
+root = monkeys['root']
+orig = root.resolve(monkeys)
+puts "Part 1: root says #{orig}"
+
+def find_human(monkey, monkeys)
+  return [] if monkey.name == 'humn'
+  op = monkey.operation.match /(?<a>\w+) (?<op>.) (?<b>\w+)/
+  return nil if op.nil?
+  parents = [monkeys[op[:a]], monkeys[op[:b]]]
+  found = parents.collect do |m|
+    found = find_human(m, monkeys)
+    found.nil? ? nil : [m.name, found]
+  end.compact.flatten
+  found.empty? ? nil : found
+end
+
+path = find_human(root, monkeys)
+last = root
+value_needed = 0
+path.each do |m|
+  parts = last.op_parts(monkeys)
+  other = parts[0].name == m ? parts[2] : parts[0]
+  other_val = other.resolve(monkeys)
+  if last == root
+    value_needed = other_val
+    last = monkeys[m]
+    next
+  end
+  case parts[1]
+  when "+"
+    value_needed = value_needed - other_val
+  when "-"
+    if other == parts.first
+      value_needed = other_val - value_needed
+    else
+      value_needed = value_needed + other_val
+    end
+  when "*"
+    value_needed = value_needed / other_val
+  when "/"
+    if other == parts.first
+      value_needed = other_val / value_needed
+    else
+      value_needed = value_needed * other_val
+    end
+  end
+  last = monkeys[m]
+end
+
+puts "Part 2: humn says #{value_needed}"
+
